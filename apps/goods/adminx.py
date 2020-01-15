37e7ac73c530cd65acdc7c49f8e9a09e04d91a07
five_sx <m13822215023@163.com>
2020-01-15 17:43:31 +0800

    fix

--- apps/goods/adminx.py
+++ apps/goods/adminx.py
@@ -16,7 +16,7 @@ class GoodsAdmin(object):
         extra = 1
         style = "tab"
 
-    inlines = [GoodsImagesInline]
+    inlines = [GoodsImagesInline] #添加数据
 
 class GoodsCategoryAdmin(object):
     list_display = ["name","category_type","parent_category","add_time"]
@@ -25,6 +25,26 @@ class GoodsCategoryAdmin(object):
 
 class GoodsBrandAdmin(object):
     list_display = ["category","image","name","desc"]
+    # 实现过滤出所有的一级类目
     def get_context(self):
         context = super(GoodsBrandAdmin,self).get_context()
+        if 'from' in context:
+            context["from"].fields['category'].queryset = GoodsCategory.objects.filter(category=1)
+            return context
+
+class BannerGoodsAdmin(object):
+    list_display = ["goods","image","index"]
+
+class HotSearchAdmin(object):
+    list_display = ["keywords", "index", "add_time"]
+
+class IndexAdAdmin(object):
+    list_display = ["category","goods"]
+
+xadmin.site.register(Goods,GoodsAdmin)
+xadmin.site.register(GoodsCategory,GoodsBrandAdmin)
+xadmin.site.register(Banner,BannerGoodsAdmin)
+xadmin.site.register(GoodsCategoryBrand,GoodsBrandAdmin)
+xadmin.site.register(HotSearchWords,HotSearchAdmin)
+xadmin.site.register(IndexAd,IndexAdAdmin)
 
